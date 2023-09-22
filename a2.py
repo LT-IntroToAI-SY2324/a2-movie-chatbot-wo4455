6f80215d299b229ed6c054fce26e3a1bbbf0891b
Will Otwell <90997466+wo4455@users.noreply.github.com>
2023-09-22 18:53:43 -0500

    Complete a2.py and a2_writeup.py

--- a2.py
+++ a2.py
@@ -14,48 +14,62 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         None if the pattern and source do not "match" ELSE A list of matched words
         (words in the source corresponding to _'s or %'s, in the pattern, if any)
     """
-    sind = 0  # current index we are looking at in source list
-    pind = 0  # current index we are looking at in pattern list
-    result: List[str] = []  # to store substitutions we will return if matched
+    sind = 0  
+    pind = 0  
+    result: List[str] = [] 
 
-    # keep checking as long as we haven't hit the end of either pattern or source while
-    # pind is still a valid index OR sind is still a valid index (valid index means that
-    # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
-        # your job is to fill out the body of this loop
-
-        # you should delete the following line
-        return ["Not done yet :)"]
+    while pind < len(pattern) or sind < len(source):
 
         # 1) if we reached the end of the pattern but not source
-
+        if pind == len(pattern) and sind < len(source):
+            return None
         # 2) if the current thing in the pattern is a %
-        # WARNING: this condition contains the bulk of the code for the assignment
-        # If you get stuck on this one, we encourage you to attempt the other conditions
-        #   and come back to this one afterwards
-
+        elif pattern[pind] == "%":
+            pind += 1
+            if pind == len(pattern):
+                result.append(" ".join(source[sind:]))
+                return result
+            else:
+                current = sind
+                while pattern[pind] != source[sind]:
+                    sind += 1
+                    if sind == len(source):
+                        return None
+                result.append(" ".join(source[current:sind]))
+            
         # 3) if we reached the end of the source but not the pattern
-
+        elif sind == len(source) and pind < len(pattern): 
+            return None
         # 4) if the current thing in the pattern is an _
-
-        # 5) if the current thing in the pattern is the same as the current thing in the
-        # source
-
+        elif pattern[pind] == "_":
+            # result.append(source[sind])
+            result += [source[sind]]
+            pind += 1
+            sind += 1
+        # 5) if the current thing in the pattern is the same as the current thing in the source
+        elif pattern[pind] == source[sind]:
+            pind += 1
+            sind += 1
         # 6) else : this will happen if none of the other conditions are met it
-        # indicates the current thing it pattern doesn't match the current thing in
-        # source
+        else:
+            return None
 
     return result
 
 
 if __name__ == "__main__":
+    print(match(["x", "y", "z"], ["x", "y", "z"]))
     assert match(["x", "y", "z"], ["x", "y", "z"]) == [], "test 1 failed"
+    print(match(["x", "z", "z"], ["x", "y", "z"]))
     assert match(["x", "z", "z"], ["x", "y", "z"]) == None, "test 2 failed"
+    print(match(["x", "y"], ["x", "y", "z"]))
     assert match(["x", "y"], ["x", "y", "z"]) == None, "test 3 failed"
+
     assert match(["x", "y", "z", "z"], ["x", "y", "z"]) == None, "test 4 failed"
     assert match(["x", "_", "z"], ["x", "y", "z"]) == ["y"], "test 5 failed"
     assert match(["x", "_", "_"], ["x", "y", "z"]) == ["y", "z"], "test 6 failed"
     assert match(["%"], ["x", "y", "z"]) == ["x y z"], "test 7 failed"
+    print(match(["x", "%", "z"], ["x", "y","a", "b", "z"]))
     assert match(["x", "%", "z"], ["x", "y", "z"]) == ["y"], "test 8 failed"
     assert match(["%", "z"], ["x", "y", "z"]) == ["x y"], "test 9 failed"
     assert match(["x", "%", "y"], ["x", "y", "z"]) == None, "test 10 failed"
@@ -68,8 +82,8 @@ if __name__ == "__main__":
         "z",
         "",
     ], "test 14 failed"
-    # this last case is a strange one, but it exposes an issue with the way we've
-    # written our match function
+    
     assert match(["x", "%", "z"], ["x", "y", "z", "z", "z"]) == None, "test 15 failed"
+    assert match(["%", "z"], ["x", "y", "w"]) == None, "test 16 failed"
 
     print("All tests passed!")
--- a2_writeup.md
+++ a2_writeup.md
@@ -13,8 +13,8 @@ This assignment is about learning and applying the while loop and iterating thro
 ## Reflection Questions
 1. What was difficult for you while completing the match function?
 
-
+Yes, it was difficult for me to complete the function. I frequently found myself trying to do it on my own and getting confused, and to either look to one of my friends or Mr. Berg's examples for guidance. I am planning on to keep practicing so that I become a little better. 
 
 2. Explain how you could use the match function for extracting information from a movie database.
 
-
+You can use this function for extracting information from a movie database by providing a pattern and a source (list of words representing a phrase). The function can match the pattern to the source, where '%' matches a sequence of zero or more words, and '_' matches any single word. If the pattern and source match, the function will return a list of matched words from the source. If there's no match, it will return None. It is similar to how it functions now, but with different parameters. 
